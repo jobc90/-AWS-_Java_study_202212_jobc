90bc9533573a367e57b7b65aa0435d3fb448aaac
jobc90 <jobc90@gmail.com>
2023-01-06 17:44:29 +0900

    로또 프로젝트 커밋

---
+++ src/j00_개인연습/J00_lotteryProject.java
@@ -0,0 +1,10 @@
+package j00_개인연습;
+
+public class J00_lotteryProject {
+
+	public static void main(String[] args) {
+
+		System.out.println("이번 주 로또번호 추첨입니다.");
+	}
+
+}
--- src/j12_배열/Array5.java
+++ src/j12_배열/Array5.java
@@ -2,18 +2,21 @@ package j12_배열;
 
 public class Array5 {
 	
+	
+	// 길이가 늘어난 새로운 배열에 기존 배열의 값을 넣는 코드
 	public static void transferArray(int[] oldArray, int [] newArray) {
 		for(int i = 0; i < oldArray.length; i++) {
 			newArray[i] = oldArray[i];
 		}
 	}
-	
+	//배열의 길이를 +1 하는 코드
 	public static int[] addArrayLength(int[] array) {
 		int[] newArray = new int[array.length + 1];
 		transferArray(array, newArray);
 		return newArray;
 	}
 	
+	//배열에 새로운 값을 넣는 코드
 	public static int [] addData(int[] array, int data) {
 		int[] newArray = addArrayLength(array);
 		newArray[array.length] = data;
