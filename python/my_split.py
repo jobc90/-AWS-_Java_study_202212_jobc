82661b9c13907a4d8093203fce2c330d52850132
jobc90 <jobc90@gmail.com>
2023-02-15 15:01:32 +0900

    230215 파이썬수업

--- python/my_split.py
+++ python/my_split.py
@@ -62,3 +62,57 @@ def process_product_names(s):
 # print(process_product_names(sample3))
 # print(process_product_names(sample4))
 # print(process_product_names(sample5))
+
+def fact(n):
+    ret = 1
+    while n>1:
+        ret *= n
+        n -= 1
+    return ret
+
+def fact_r(n):
+    if n==2:
+        return n
+    return fact_r(n-1)*n
+
+def perm_f(n, r):
+    return fact(n)/fact(n-r)
+
+def combi_f(n, r):
+    return fact(n)/(fact(n-r)*fact(r))
+
+def count_with_replacement_order(pool, sel_count):  # all
+    if sel_count == 1:
+        return len(pool), [[x] for x in pool]
+    a, b = count_with_replacement_order(pool, sel_count-1)
+    r = []
+    for x in pool:
+        for b_i in b:
+            r.append(b_i+[x])
+    return len(r), r    
+
+def perm_pool(pool, r):
+    if r == 1:
+        return len(pool), [[x] for x in pool]
+    ret = []
+    for x in pool:
+        pool_copy = [y for y in pool]
+        pool_copy.remove(x)
+        a, b = perm_pool(pool_copy, r-1)
+        for b_i in b:
+            ret.append(b_i+[x])
+    return len(ret), ret
+
+def combi_pool(pool, r):
+    a , b = perm_pool(pool, r)
+    r = []
+    for b_i in b:
+        r.append(set(b_i))
+    f = []
+    for r_i in r:
+        f.append(r_i)
+        while r_i in r:
+            r.remove(r_i)
+    if r:
+        f.append(r[0])
+    return len(f), f
